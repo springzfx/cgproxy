5c16fdfb9f1d8b07a2a2cf72fbf8f9c4d8db1146
fancy <sciencezfx@tju.edu.cn>
2020-05-01 11:55:57 +0800

    add v2ray config example

---
+++ v2ray_config/merge.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+jq -rs 'reduce .[] as $item ({}; . + $item  + {inbounds: (.inbounds + $item.inbounds)} + {outbounds: ($item.outbounds + .outbounds)})' *.json |sudo tee /etc/v2ray/config.json
---
+++ v2ray_config/readme.md
@@ -0,0 +1,1 @@
+Fill `06_outbounds_myproxy.json` with your vmess proxy config with tag `outBound_PROXY`.

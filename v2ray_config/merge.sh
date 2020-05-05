3b4b67df335a4fb1f791dda32a689f3958e1d13b
fancy <springzfx@gmail.com>
2020-05-05 18:11:02 +0800

    small change

--- v2ray_config/merge.sh
+++ v2ray_config/merge.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-jq -rs 'reduce .[] as $item ({}; . + $item  + {inbounds: (.inbounds + $item.inbounds)} + {outbounds: ($item.outbounds + .outbounds)})' *.json |sudo tee /etc/v2ray/config.json
+jq -rs 'reduce .[] as $item ({}; . + $item  + {inbounds: (.inbounds + $item.inbounds)} + {outbounds: ($item.outbounds + .outbounds)})' *.json |sudo tee /etc/v2ray/config.json > /dev/null

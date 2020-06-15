3f1bee745bd265553f0d2c9bb73c53bac2b62099
Fancy Zhang <springzfx@gmail.com>
2020-06-15 21:46:47 +0800

    replace typedef with using decltype

--- src/execsnoop.h
+++ src/execsnoop.h
@@ -15,7 +15,8 @@ void handle_events(void *cb_cookie, void *data, int data_size);
 int execsnoop();
 
 extern "C" void startThread(function<int(int)> c, promise<void> _status);
-typedef void startThread_t(function<int(int)>, promise<void>);
+// typedef void startThread_t(function<int(int)>, promise<void>);
+using startThread_t=decltype(startThread);
 startThread_t *_startThread; // only for dlsym()
 
 } // namespace CGPROXY::EXECSNOOP
